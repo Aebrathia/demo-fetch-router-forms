ff78706583de3686be83284e7a3060ee514252bb
Andrea Maugars <andrea.maugars@gmail.com>
2019-10-22 16:34:32 +0200

    Fetch movies and display 20 most popular

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// /discover/movie?sort_by=popularity.desc
+// ?api_key=7f077937236d1ffe1a9deeb64a9d2a38
+// "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=7f077937236d1ffe1a9deeb64a9d2a38')
+    const data = await response.json();
+    this.setState({ movies: data.results });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.movies.map(movie => (
+          <div key={movie.id} className="movie">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
